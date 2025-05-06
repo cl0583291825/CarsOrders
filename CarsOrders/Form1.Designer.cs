71ae066d549c20ca447cfecdb6afad2b2cc6272c
cl0583291825 <cl0583291825@gmail.com>
2025-05-06 14:52:57 +0300

    Add project files.

---
+++ CarsOrders/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace CarsOrders
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            m_CompanyNameTB = new TextBox();
+            m_CarNumber = new TextBox();
+            m_CompanyHpTB = new TextBox();
+            m_CarModel = new TextBox();
+            m_CarColor = new TextBox();
+            m_AddCarBtn = new Button();
+            m_SaveOrderBtn = new Button();
+            DisplayFeedbakXml = new Button();
+            m_InsertTextLabel = new Label();
+            m_SearchCarTB = new TextBox();
+            m_SearchBtn = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1301, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 32);
+            label1.TabIndex = 0;
+            label1.Text = "שם החברה";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1301, 236);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 32);
+            label2.TabIndex = 1;
+            label2.Text = "ח.פ חברה";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1281, 326);
+            label3.Name = "label3";
+            label3.Size = new Size(155, 32);
+            label3.TabIndex = 2;
+            label3.Text = "מספרי רכבים:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1241, 420);
+            label4.Name = "label4";
+            label4.Size = new Size(121, 32);
+            label4.TabIndex = 3;
+            label4.Text = "מספר רכב";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1241, 500);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 32);
+            label5.TabIndex = 4;
+            label5.Text = "דגם";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1241, 573);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 32);
+            label6.TabIndex = 5;
+            label6.Text = "צבע";
+            // 
+            // m_CompanyNameTB
+            // 
+            m_CompanyNameTB.Location = new Point(1074, 162);
+            m_CompanyNameTB.Name = "m_CompanyNameTB";
+            m_CompanyNameTB.Size = new Size(200, 39);
+            m_CompanyNameTB.TabIndex = 6;
+            // 
+            // m_CarNumber
+            // 
+            m_CarNumber.Location = new Point(1021, 413);
+            m_CarNumber.Name = "m_CarNumber";
+            m_CarNumber.Size = new Size(200, 39);
+            m_CarNumber.TabIndex = 7;
+            // 
+            // m_CompanyHpTB
+            // 
+            m_CompanyHpTB.Location = new Point(1074, 229);
+            m_CompanyHpTB.Name = "m_CompanyHpTB";
+            m_CompanyHpTB.Size = new Size(200, 39);
+            m_CompanyHpTB.TabIndex = 8;
+            // 
+            // m_CarModel
+            // 
+            m_CarModel.Location = new Point(1021, 493);
+            m_CarModel.Name = "m_CarModel";
+            m_CarModel.Size = new Size(200, 39);
+            m_CarModel.TabIndex = 9;
+            // 
+            // m_CarColor
+            // 
+            m_CarColor.Location = new Point(1021, 573);
+            m_CarColor.Name = "m_CarColor";
+            m_CarColor.Size = new Size(200, 39);
+            m_CarColor.TabIndex = 10;
+            // 
+            // m_AddCarBtn
+            // 
+            m_AddCarBtn.Location = new Point(935, 642);
+            m_AddCarBtn.Name = "m_AddCarBtn";
+            m_AddCarBtn.Size = new Size(150, 46);
+            m_AddCarBtn.TabIndex = 11;
+            m_AddCarBtn.Text = "הוסף רכב";
+            m_AddCarBtn.UseVisualStyleBackColor = true;
+            m_AddCarBtn.Click += m_AddCarBtn_Click;
+            // 
+            // m_SaveOrderBtn
+            // 
+            m_SaveOrderBtn.Location = new Point(805, 733);
+            m_SaveOrderBtn.Name = "m_SaveOrderBtn";
+            m_SaveOrderBtn.Size = new Size(150, 46);
+            m_SaveOrderBtn.TabIndex = 12;
+            m_SaveOrderBtn.Text = "שמור";
+            m_SaveOrderBtn.UseVisualStyleBackColor = true;
+            m_SaveOrderBtn.Click += m_SaveOrderBtn_Click;
+            // 
+            // DisplayFeedbakXml
+            // 
+            DisplayFeedbakXml.Location = new Point(496, 926);
+            DisplayFeedbakXml.Name = "DisplayFeedbakXml";
+            DisplayFeedbakXml.Size = new Size(459, 46);
+            DisplayFeedbakXml.TabIndex = 13;
+            DisplayFeedbakXml.Text = "התקבל משוב להזמנה - לחץ לתצוגה";
+            DisplayFeedbakXml.UseVisualStyleBackColor = true;
+            DisplayFeedbakXml.Visible = false;
+            // 
+            // m_InsertTextLabel
+            // 
+            m_InsertTextLabel.AutoSize = true;
+            m_InsertTextLabel.Location = new Point(471, 162);
+            m_InsertTextLabel.Name = "m_InsertTextLabel";
+            m_InsertTextLabel.Size = new Size(244, 32);
+            m_InsertTextLabel.TabIndex = 14;
+            m_InsertTextLabel.Text = "הזן מספר רכב לחיפוש:";
+            // 
+            // m_SearchCarTB
+            // 
+            m_SearchCarTB.Location = new Point(127, 159);
+            m_SearchCarTB.Name = "m_SearchCarTB";
+            m_SearchCarTB.Size = new Size(326, 39);
+            m_SearchCarTB.TabIndex = 15;
+            // 
+            // m_SearchBtn
+            // 
+            m_SearchBtn.Location = new Point(45, 222);
+            m_SearchBtn.Name = "m_SearchBtn";
+            m_SearchBtn.Size = new Size(150, 46);
+            m_SearchBtn.TabIndex = 16;
+            m_SearchBtn.Text = "חפש";
+            m_SearchBtn.UseVisualStyleBackColor = true;
+            m_SearchBtn.Click += m_SearchBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1745, 1003);
+            Controls.Add(m_SearchBtn);
+            Controls.Add(m_SearchCarTB);
+            Controls.Add(m_InsertTextLabel);
+            Controls.Add(DisplayFeedbakXml);
+            Controls.Add(m_SaveOrderBtn);
+            Controls.Add(m_AddCarBtn);
+            Controls.Add(m_CarColor);
+            Controls.Add(m_CarModel);
+            Controls.Add(m_CompanyHpTB);
+            Controls.Add(m_CarNumber);
+            Controls.Add(m_CompanyNameTB);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "CarsOrders";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox m_CompanyNameTB;
+        private TextBox m_CarNumber;
+        private TextBox m_CompanyHpTB;
+        private TextBox m_CarModel;
+        private TextBox m_CarColor;
+        private Button m_AddCarBtn;
+        private Button m_SaveOrderBtn;
+        private Button DisplayFeedbakXml;
+        private Label m_InsertTextLabel;
+        private TextBox m_SearchCarTB;
+        private Button m_SearchBtn;
+    }
+}
---
+++ CarsOrders/Form1.cs
@@ -0,0 +1,318 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using Microsoft.Data.SqlClient;
+using System.Xml;
+using static CarsOrders.Form1;
+using System.Xml.Linq;
+using System.Globalization;
+using System;
+using static Azure.Core.HttpHeader;
+
+namespace CarsOrders
+{
+    public partial class Form1 : Form
+    {
+
+        public class Car
+        {
+            public string CarNumber { get; set; }
+
+            public string CarModel { get; set; }
+
+            public string CarColor { get; set; }
+
+            public Car(string carNumber, string carModel, string carColor)
+            {
+                CarNumber = carNumber;
+                CarModel = carModel;
+                CarColor = carColor;
+            }
+        }
+
+        public class CarOrder
+        {
+            public int Id { get; set; }
+
+            public string CompanyName { get; set; }
+
+            public string CompanyHp { get; set; }
+
+            public List<Car> Cars { get; set; }
+        }
+
+        public CarOrder Order { get; set; }
+
+        public readonly string ConnectionString = "Server=localhost\\LSQL;Database=CarsTest;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public string PathFile = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\Downloads";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Order = new CarOrder();
+            Order.Cars = new List<Car>();
+        }
+
+        private void m_AddCarBtn_Click(object sender, EventArgs e)
+        {
+            Order.Cars.Add(new Car(m_CarNumber.Text, m_CarModel.Text, m_CarColor.Text));
+            m_CarNumber.Clear();
+            m_CarModel.Clear();
+            m_CarColor.Clear();
+        }
+
+        private void m_SaveOrderBtn_Click(object sender, EventArgs e)
+        {
+            Order.CompanyName = m_CompanyNameTB.Text;
+            Order.CompanyHp = m_CompanyHpTB.Text;
+            if (!string.IsNullOrEmpty(m_CarNumber.Text) || !string.IsNullOrEmpty(m_CarModel.Text) || !string.IsNullOrEmpty(m_CarColor.Text))
+            {
+                Order.Cars.Add(new Car(m_CarNumber.Text, m_CarModel.Text, m_CarColor.Text));
+                m_CarNumber.Clear();
+                m_CarModel.Clear();
+                m_CarColor.Clear();
+            }
+            SaveOrderOnDB();
+        }
+
+        private void SaveOrderOnDB()
+        {
+            try
+            {
+                using SqlConnection conn = new(ConnectionString);
+                conn.Open();
+                using SqlCommand cmdOrder = new("INSERT INTO Orders (CompanyName, CompanyHp, Date) OUTPUT INSERTED.Id VALUES (@name, @number, @date)", conn);
+                cmdOrder.Parameters.AddWithValue("@name", Order.CompanyName);
+                cmdOrder.Parameters.AddWithValue("@number", Order.CompanyHp);
+                cmdOrder.Parameters.AddWithValue("@date", DateTime.Now);
+                Order.Id = (int)cmdOrder.ExecuteScalar();
+
+
+                string insertCarQuery = "INSERT INTO Cars (OrderId, CarNumber, Model, Color) VALUES (@id, @number, @model, @color)";
+                using SqlCommand cmdCar = new(insertCarQuery, conn);
+
+                cmdCar.Parameters.Add("@id", SqlDbType.Int);
+                cmdCar.Parameters.Add("@number", SqlDbType.NVarChar);
+                cmdCar.Parameters.Add("@model", SqlDbType.NVarChar);
+                cmdCar.Parameters.Add("@color", SqlDbType.NVarChar);
+                foreach (var c in Order.Cars)
+                {
+                    cmdCar.Parameters["@id"].Value = Order.Id;
+                    cmdCar.Parameters["@number"].Value = c.CarNumber;
+                    cmdCar.Parameters["@model"].Value = c.CarModel;
+                    cmdCar.Parameters["@color"].Value = c.CarColor;
+
+                    cmdCar.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                m_CompanyNameTB.Clear();
+                m_CompanyHpTB.Clear();
+                m_CarNumber.Clear();
+                m_CarModel.Clear();
+                m_CarColor.Clear();
+            }
+
+            ShowSaveToXmlBtn();
+        }
+
+        private void ShowSaveToXmlBtn()
+        {
+            Button saveToXmlBtn = new Button();
+            saveToXmlBtn.Text = "הנפק קובץ Xml";
+            saveToXmlBtn.Visible = true;
+            saveToXmlBtn.Width = 200;
+            saveToXmlBtn.Height = m_SaveOrderBtn.Height;
+            saveToXmlBtn.Location = new Point(m_SaveOrderBtn.Location.X, m_SaveOrderBtn.Location.Y + 80);
+            saveToXmlBtn.Click += SaveToXmlBtn_Click;
+            this.Controls.Add(saveToXmlBtn);
+        }
+
+        private void SaveToXmlBtn_Click(object? sender, EventArgs e) => SaveOrderToXml();
+
+        private void SaveOrderToXml()
+        {
+            var order = Order;
+
+            XmlDocument doc = new XmlDocument();
+
+            XmlElement orderRequest = doc.CreateElement("OrderRequest");
+
+            XmlElement idElement = doc.CreateElement("OrderID");
+            idElement.InnerText = order.Id.ToString();
+            orderRequest.AppendChild(idElement);
+
+            XmlElement dateElement = doc.CreateElement("OrderDate");
+            dateElement.InnerText = DateTime.Now.ToString();
+            orderRequest.AppendChild(dateElement);
+
+            XmlElement descElement = doc.CreateElement("OrderDescription");
+            descElement.InnerText = order.CompanyName;
+            orderRequest.AppendChild(descElement);
+
+            XmlElement companyElement = doc.CreateElement("CompanyHp");
+            companyElement.InnerText = order.CompanyHp;
+            orderRequest.AppendChild(companyElement);
+
+            XmlElement carsElement = doc.CreateElement("Cars");
+
+            foreach (var c in order.Cars)
+            {
+                XmlElement carElement = doc.CreateElement("Car");
+
+                XmlElement numberElement = doc.CreateElement("CarNumber");
+                numberElement.InnerText = c.CarNumber;
+                carElement.AppendChild(numberElement);
+
+                XmlElement modelElement = doc.CreateElement("Model");
+                modelElement.InnerText = c.CarModel;
+                carElement.AppendChild(modelElement);
+
+                XmlElement colorElement = doc.CreateElement("Color");
+                colorElement.InnerText = c.CarColor;
+                carElement.AppendChild(colorElement);
+
+                carsElement.AppendChild(carElement);
+            }
+
+            orderRequest.AppendChild(carsElement);
+            doc.AppendChild(orderRequest);
+
+            var filePath = Path.Combine(PathFile, $"Order_{order.Id}.xml");
+            doc.Save(filePath);
+            System.Diagnostics.Process.Start("explorer.exe", $"/select,\"{filePath}\"");
+
+            ShowBtnDisplayFeedbackXml();
+        }
+
+        public async void ShowBtnDisplayFeedbackXml()
+        {
+            await Task.Delay(20000);
+            DisplayFeedbakXml.Visible = true;
+            DisplayFeedbakXml.Click += DisplayFeedbakXmlBtn_Click;
+        }
+
+        public void DisplayFeedbakXmlBtn_Click(object sender, EventArgs e)
+        {
+            var filePath = Path.Combine(PathFile, $"CarsRequestFeedback (2).xml");
+            ReadAndSaveOnDBFeedbackXml(filePath);
+            DisplayFeedbakXml.Visible = false;
+        }
+
+        public void ReadAndSaveOnDBFeedbackXml(string xmlFilePath)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(xmlFilePath);
+
+                var orderResponse = doc.Descendants("OrderResponse").FirstOrDefault();
+
+                int orderId = int.Parse(orderResponse.Element("Order")?.Value ?? "0");
+                DateTime feedbackDate;
+                string feedbackDatestr = orderResponse.Element("FeedbackDate")?.Value;
+                var success = DateTime.TryParseExact(
+                    feedbackDatestr,
+                    "dd-MM-yyyy'T'HH:mm",
+                    System.Globalization.CultureInfo.InvariantCulture,
+                    System.Globalization.DateTimeStyles.None,
+                    out feedbackDate
+                );
+                feedbackDate = success ? feedbackDate : DateTime.Now;
+
+                int feedbackType = int.Parse(orderResponse.Element("FeedbackType")?.Value ?? "0");
+                string comment = orderResponse.Element("Comment")?.Value ?? "";
+
+                string message = $"מספר הזמנה: {orderId}\r\n" +
+                                 $"תאריך משוב: {feedbackDate}\r\n" +
+                                 $"סוג משוב: {feedbackType}\r\n" +
+                                 $"הערה: {comment}";
+
+                MessageBox.Show(message, "פרטי ההזמנה", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                using SqlConnection conn = new(ConnectionString);
+                conn.Open();
+
+                string insertQuery = @"INSERT INTO Feedback (OrderId, FeedbackDate, FeedbackType, Comment)
+                       VALUES (@OrderId, @FeedbackDate, @FeedbackType, @Comment)";
+
+                using SqlCommand cmd = new SqlCommand(insertQuery, conn);
+                cmd.Parameters.AddWithValue("@OrderId", orderId);
+                cmd.Parameters.AddWithValue("@FeedbackDate", feedbackDate);
+                cmd.Parameters.AddWithValue("@FeedbackType", feedbackType);
+                cmd.Parameters.AddWithValue("@Comment", comment);
+
+                cmd.ExecuteNonQuery();
+
+                insertQuery = @"INSERT INTO FeedbackType (Type, Description)
+                       VALUES (@type, @description)";
+
+                using SqlCommand cmdFeedbackType = new SqlCommand(insertQuery, conn);
+                cmdFeedbackType.Parameters.AddWithValue("@type", feedbackType);
+                cmdFeedbackType.Parameters.AddWithValue("@description", comment);
+
+                cmdFeedbackType.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void m_SearchBtn_Click(object sender, EventArgs e)
+        {
+            var input = m_SearchCarTB.Text;
+            if (string.IsNullOrEmpty(input))
+                MessageBox.Show("נא הזן טקסט לחיפוש", "אזהרה", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                DoSearchCarOnDBAndDisplayDetailse(input);
+        }
+
+        private void DoSearchCarOnDBAndDisplayDetailse(string text)
+        {
+            using SqlConnection conn = new(ConnectionString);
+            conn.Open();
+
+            string searchQuery = @"SELECT c.CarNumber, c.OrderId, c.Model, c.Color, o.CompanyName, o.CompanyHp, o.Date
+                                 FROM Cars c
+                                 LEFT JOIN Orders o ON c.OrderId = o.Id
+                                 WHERE c.CarNumber = @CarNumber";
+
+            using SqlCommand cmd = new SqlCommand(searchQuery, conn);
+            cmd.Parameters.AddWithValue("@CarNumber", text);
+
+            using SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                string message = $"מספר רכב: {reader["CarNumber"]}";
+
+                if (reader["OrderId"] != DBNull.Value)
+                {
+                    message +=
+                        $"\nמקושר להזמנה: {reader["OrderId"]}" +
+                        $"\nחברה: {reader["CompanyName"]}" +
+                        $"\nטלפון: {reader["CompanyHp"]}" +
+                        $"\nתאריך: {Convert.ToDateTime(reader["Date"]).ToString("dd/MM/yyyy HH:mm")}";
+                }
+                else
+                {
+                    message += "\nאין הזמנה מקושרת לרכב זה.";
+                }
+
+                MessageBox.Show(message, "פרטי רכב:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("הרכב לא קיים במערכת");
+            }
+        }
+    }
+}
